a79ffd5a37b4848835748eab82837bb6c994c4c1
fareen341 <59610617+fareen341@users.noreply.github.com>
2021-10-23 09:41:22 +0530

    Update settings.py

--- WatchShop/settings.py
+++ WatchShop/settings.py
@@ -157,5 +157,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST=  'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT= 587
-EMAIL_HOST_USER= "fareenansari33@gmail.com"
-EMAIL_HOST_PASSWORD= "yoususinamongus@0055"
+EMAIL_HOST_USER= "your email"
+EMAIL_HOST_PASSWORD= "pswd"
